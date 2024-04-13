4a7db64a4f792e16d8b3f30eda28c38d2c50f355
DedN4 <acslegendados16@gmail.com>
2024-04-13 19:43:03 -0300

    CONCLUSÃO DO EXERCÍCIO

--- bank01.py
+++ bank01.py
@@ -1,7 +1,16 @@
 
 # SISTEMA DE BANKING - DEIVID BANK
 
-init_message = """
+dados = {
+    "saldo": 0,
+    "valor_limite": 500,
+    "saques_atual": 0,
+    "limite_saque": 3   
+}
+
+extrato = []
+
+incio = """
 
 ====================================
 -     DEIVID BANKING (ONLINE)      -
@@ -13,119 +22,103 @@ init_message = """
 
 """
 
-username = ""
-cpf_user = ""
-pin_user = ""
-
-dadosCliente = {
-    "Username": username,
-    "CPF": pin_user,
-    "Senha": cpf_user,
-    "SAQUE": [],
-    "DEPÓSITO": []
-}
-
-signin_message = f"""
-
-======================================
--     DEIVID BANKING (ONLINE)        -
-======================================
--                                    
--       ! Realize seu login:           
--                                    
--       -> NOME  [{username}]           
--       -> CPF   [{cpf_user}]           
--       -> PIN   [{pin_user}]           
--                                    
-======================================
- 
-"""
-
-welcome_message = f"""
+menu = """
 
 ====================================
 -     DEIVID BANKING (ONLINE)      -
 ====================================
 -                                  
--     SEJA MUITO BEM-VINDO !!      
--     -> {username}      
+-     O que você precisa  ??       
+-                                  
+-     1 -> DEPÓSITO                   
+-     2 -> SAQUE                
+-     3 -> EXTRATO                 
+-     4 -> SAIR                   
 -                                  
 ====================================
 
-"""
+R: """
 
-option_message = """
+extrato_fim = '' 
+  
+def printExtrato():
+    if len(extrato) == 0:
+        "Não foram realizadas movimentações." 
+    else:
+        for movimentacao in extrato:
+            global extrato_fim 
+            extrato_fim = extrato_fim + "" + f"-     {movimentacao}"
+            
+    
 
+while True:
+    
+    opcao = input(menu)
+    print(opcao)
+    # PROCESSOS DE DEPÓSITO
+    if opcao == "1":
+        valor = float(input("Informe o valor do depósito: "))
+        if valor > 0:
+            dados["saldo"] = dados["saldo"] + valor
+            extrato.append(f"Depósito: R$ {valor}\n")
+            print(f"Depósito: R$ {valor}\nSaldo Atual: R$ {dados['saldo']}\n")
+        else:
+            print("Operação falhou! O valor informado é inválido.")
+         
+    # ME SEGURANDO PARA NÃO USAR FUNÇÕES :V
+
+    # PROCESSO PARA SACAR   
+    elif opcao == "2":
+        valor = float(input("Informe o valor do saque: "))
+
+        excedeu_saldo = valor > dados["saldo"]
+        # VALOR MAIOR QUE O SAQUE
+        excedeu_limite = valor > dados["valor_limite"]
+        # VALOR MAIOR QUE O LIMITE
+        excedeu_saques = dados["saques_atual"] >= dados["limite_saque"]
+        # VALOR MAIOR/IGUAL A LIMITES DE SAQUES
+        
+        if excedeu_saldo:
+            print("Você não possui saldo suficiente.")
+
+        elif excedeu_limite:
+            print("O valor indicado para saque ultrapassou o limite.")
+
+        elif excedeu_saques:
+            print("Você excedeu a quantidade máxima de saques.")
+
+        elif valor > 0:
+            dados["saldo"] = dados["saldo"] - valor
+            extrato.append(f"Saque: R$ {valor:.2f}\n")
+            dados["saques_atual"] += 1
+            print(f"Valor do saque: R$ {valor}\nSaldo Atual: R$ {dados['saldo']}\n")
+
+        else:
+            print("O valor informado é inválido.")
+
+    elif opcao == "3":  
+        printExtrato()     
+        print(
+f"""
 ====================================
 -     DEIVID BANKING (ONLINE)      -
 ====================================
--                                  -
--     O que você precisa  ??       -
--                                  -
--     1 -> SAQUE                   -
--     2 -> DEPÓSITO                -
--     3 -> EXTRATO                 -
--     4 -> SAIR                    -
--                                  -
+-                                  
+-     Saldo: R$ {dados['saldo']:.2f}   
+-     
+-     SUAS MOVIMENTAÇÕES:
+-     \n{extrato_fim}-
+-
+-                                  
 ====================================
+""")    
+        extrato_fim = "" # FAZENDO RESET PARA NÃO DUPLICAR VALORES
 
-R: 
-"""
+    elif opcao == "4":
+        break
 
-def menu_iniciar():
-    print(init_message)
+    else:
+        print("Operação inválida, por favor selecione novamente a operação desejada.")
+        
 
-    i = 0
-    while i <= 3:
-
-        global username, cpf_user, pin_user
-
-        # PEDINDO NOME
-        username = input(signin_message + "\nDigite seu nome: ").strip()
-        i += 1
-
-        # PEDINDO CPF
-        cpf_user = input(signin_message + "\nDigite seu CPF: ").strip()
-        i += 1
-
-        pin_user = input(signin_message + "\nDigite sua senha: ").strip()
-        i += 1
-
-        return signin_message
-
-
-
-menu_iniciar()
-
-
-# -> USUÁRIO
-# SAQUE, EXTRATO E DEPÓSITO
-
-
-
-
-
-
-
-
-
-
-
-
-
-# ------
-# DEPÓSITO:
-# VALORES POSITIVOS.
-# ARMAZENAR EM 'BD'
-
-# ------
-# SAQUE:
-# 3 SAQUES
-# VALORES DE ATÉ 500,0 (CDA)
-# ARMAZENAR EM 'BD'
-
-
-# ------
-# EXTRATO:
-# INFORMAÇÕES = 'BD'
-# FORMATAÇÃO MONETÁRIA `R$ XXXX.XX`
+        
